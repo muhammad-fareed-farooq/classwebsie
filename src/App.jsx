1d7a32d169d5cff84d374cf536bbe7f2e04a092d
muhammad-fareed-farooq <144248333+muhammad-fareed-farooq@users.noreply.github.com>
2024-01-17 21:22:07 +0500

    Add files via upload
    0k

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Navbar } from './components/Navbar'
+import { Products } from './components/Products'
+
+function App() {
+
+  return (
+    <div className='space-y-12'>
+      <Navbar />
+      <Products />
+    </div>
+  )
+}
+
+export default App
